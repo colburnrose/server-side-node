efdb5d8585d4404dcb2280b617bea12b2291f777
colburncodes <colburnsanders@gmail.com>
2021-01-30 10:51:34 -0800

    integration testing

--- express_testing/test/app.test.js
+++ express_testing/test/app.test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { json } = require("express");
 const supertest = require("supertest");
 const app = require("../app");
 
@@ -8,11 +9,36 @@ describe("Express App", () => {
   });
 });
 
-describe("GET /quotient", () => {
-  it("8/4 should be 2", () => {
+// describe("GET /quotient", () => {
+//   it("8/4 should be 2", () => {
+//     return supertest(app)
+//       .get("/quotient")
+//       .query({ a: 8, b: 4 })
+//       .expect(200, "8 divided by 4 is 2");
+//   });
+//   it(`should return 400 if 'a' is missing`, () => {
+//     return supertest(app)
+//       .get("/quotient")
+//       .query({ b: 4 })
+//       .expect(400, "value is needed for a");
+//   });
+// });
+
+describe("GET /generate", () => {
+  it("should generate an array of 5", () => {
     return supertest(app)
-      .get("/quotient")
-      .query({ a: 8, b: 4 })
-      .expect(200, "8 divided by 4 is 2.");
+      .get("/generate")
+      .query({ n: 5 })
+      .expect(200)
+      .expect("Content-type", /json/)
+      .then((res) => {
+        // make sure to get the array
+        expect(res.body.initial).to.be.an("array");
+        // array must not be empty
+        expect(res.body.initial).to.have.lengthOf.at.least(5);
+        // the assertion fails
+        // expect(res.body).to.eql([1, 2, 3, 4, 5]);
+        expect(res.body).to.include([]);
+      });
   });
 });
